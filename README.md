d05e015b9964bc062245c913d434989e2e6f0b96
Alex Queudot <alex@Alexs-MacBook-Pro.local>
2018-04-03 12:13:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ios-leaderboard-firebase
---
+++ leaderboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  leaderboard
+//
+//  Created by Alex Queudot on 03/04/2018.
+//  Copyright © 2018 ENTI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
